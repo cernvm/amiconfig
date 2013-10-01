066dc8968c1e8c5f0d2e9a7d5557eb098aefa73e
Jakob Blomer <jblomer@cern.ch>
2013-10-01 18:03:38 +0200

    rename appliance plugin to appliance-agent

--- cernvm/amiconfig/plugins/appliance-agent.py
+++ cernvm/amiconfig/plugins/appliance-agent.py
@@ -9,19 +9,18 @@ from amiconfig.lib import util
 from amiconfig.plugin import AMIPlugin
 
 class AMIConfigPlugin(AMIPlugin):
-    name = 'appliance'
+    name = 'appliance-agent'
 
     def configure(self):
         """
-        [rpath]
-        # Called rpath for historical reasons
-        rap-password=password
+        [appliance-agent]
+        password=password
         """
 
-        cfg = self.ud.getSection('rpath')
+        cfg = self.ud.getSection('appliance-agent')
         
-        if 'rap-password' in cfg:
-          util.call(['htpasswd', '-mb', "/usr/libexec/cernvm-appliance-agent/.htpasswd %s" % (cfg['rap-password'])])
+        if 'password' in cfg:
+          util.call(['htpasswd', '-mb', "/usr/libexec/cernvm-appliance-agent/.htpasswd %s" % (cfg['password'])])
           util.call(['mkdir', '-p', '/var/lib/cernvm-appliance-agent'])
           util.call(['touch', '/var/lib/cernvm-appliance-agent/password_set'])
           util.call(['chown', '-R', 'cernvm-appliance-agent', '/var/lib/cernvm-appliance-agent'])
